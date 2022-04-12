17f9cb9f2f5205c8baf5d817324aea5eab53395d
TulaOff <offenhendenpedro@gmail.com>
2022-04-12 09:50:19 -0300

    change requirements

--- facturacion_project/settings.py
+++ facturacion_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,3 +153,5 @@ MEDIA_URL_3 = '/facturas/facturas_abril/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
